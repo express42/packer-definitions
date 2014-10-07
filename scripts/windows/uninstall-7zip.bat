84e228ec8b4a2c0d2d006c5e366edb7b0cbd7cd4
Alexander Titov <titoff.a@gmail.com>
2014-10-07 12:46:26 +0400

    added support for windows 2008 r2 and parallels packer provider, initial support for win 2012, this definition is working with evaluation version of windows

---
+++ scripts/windows/uninstall-7zip.bat
@@ -0,0 +1,25 @@
+@setlocal EnableDelayedExpansion EnableExtensions
+@for %%i in (a:\_packer_config*.cmd) do @call "%%~i"
+@if not defined PACKER_DEBUG echo off
+
+echo ==^> Uninstalling 7zip
+if exist "%SystemRoot%\7z.exe" del /f "%SystemRoot%\7z.exe"
+if exist "%SystemRoot%\7z.dll" del /f "%SystemRoot%\7z.dll"
+
+:exit0
+
+@ping 127.0.0.1
+@ver>nul
+
+@goto :exit
+
+:exit1
+
+@ping 127.0.0.1
+@verify other 2>nul
+
+:exit
+
+@echo ==^> Script exiting with errorlevel %ERRORLEVEL%
+@exit /b %ERRORLEVEL%
+
